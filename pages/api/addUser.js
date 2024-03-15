3ded3546247a54d141e3abb3ffd3b5a31c3e8e4a
Sarvesh Sandeep Rasal <justanothersarvesh@gmail.com>
2024-03-16 02:33:26 +0530

    Just a little update

--- pages/api/addUser.js
+++ pages/api/addUser.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
             res.status(201).json({message: 'User Created Successfully'});
         } catch (error) {
             console.error('Error creating user: ', error.message);
-            res.status(500).json({error: 'Internal Server Error'});
+            res.status(500).json({error: 'Internal Server Error'}, error.message);
         }
     } else {
             res.status(405).json({error: 'Method not allowed'});
